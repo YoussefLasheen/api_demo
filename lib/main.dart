938627c67478887eb42d700fb8c468d9588050eb
unknown <ylasheen5@gmail.com>
2020-04-01 09:40:02 +0200

    Set localization local to Arabic

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
-import 'package:api_demo/screens/course_details_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'package:api_demo/screens/course_details_screen.dart';
 
 void main() {
-  runApp(MaterialApp(home: CourseDetailsScreen(),));
+  runApp(
+    MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('ar'),
+      ],
+      locale: Locale('ar'),
+      home: CourseDetailsScreen(),
+    ),
+  );
 }
