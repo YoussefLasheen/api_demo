5a324321952fa28f2404627c8d82680a2013a9ba
unknown <ylasheen5@gmail.com>
2020-04-01 09:49:00 +0200

    Add LongDescSection

---
+++ lib/screens/course_details_screen/components/long_desc_section/long_desc_section.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'package:auto_size_text/auto_size_text.dart';
+
+class LongDescSection extends StatelessWidget {
+  final Map data;
+  const LongDescSection({Key key, this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Text(
+            'عن الدورة',
+            style:
+                TextStyle(fontWeight: FontWeight.bold, color: Colors.black54),
+          ),
+        ),
+        Expanded(
+          flex: 4,
+          child: AutoSizeText(
+            data['long_desc'],
+            style: TextStyle(
+                color: Colors.black45,
+                fontSize: 50),
+          ),
+        )
+      ],
+    );
+  }
+}
